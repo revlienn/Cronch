ac124c9997afd2ee5ec047f75bb5056d7a7a1820
revlienn <revnathalia@gmail.com>
2025-10-16 09:36:38 +0100

    Total nutrition display

--- client/src/app/foods/cart/cart.ts
+++ client/src/app/foods/cart/cart.ts
@@ -1,15 +1,25 @@
-import { Component, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { FoodService } from '../../services/food-service';
-import { FoodCardFacts } from '../../../types/Food';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
-  imports: [JsonPipe],
+  imports: [],
   templateUrl: './cart.html',
   styleUrl: './cart.css'
 })
-export class Cart{
+export class Cart {
 
   protected foodService=inject(FoodService);
+  protected total=computed(()=>{
+    const cartItems=this.foodService.list();
+    return cartItems.reduce((totals,item)=>({
+      calories:totals.calories+item.calories,
+      protein:parseFloat((totals.protein+item.protein).toFixed(2))
+    }),
+    {calories:0,protein:0}
+  )
+  })
+
+  constructor(){
+  }
 }
