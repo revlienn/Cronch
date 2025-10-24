a87b9aadd4236bbfc86549d31113b1baad027e80
revlienn <revnathalia@gmail.com>
2025-10-24 12:04:27 +0100

    Total per day

--- client/src/app/foods/food-diary/food-diary.ts
+++ client/src/app/foods/food-diary/food-diary.ts
@@ -1,17 +1,17 @@
-import { Component, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
 import { FoodService } from '../../services/food-service';
 import { FoodCartItem } from '../../../types/Food';
 
-interface IDayText{
-  [key:number]:string
+interface IDayText {
+  [key: number]: string
 }
 
-const DayText:IDayText={
-  0:'Sunday',
-  1:'Monday',
-  2:'Tuesday',
-  3:'Wednesday',
-  5:'Friday'
+const DayText: IDayText = {
+  0: 'Sunday',
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  5: 'Friday'
 };
 
 @Component({
@@ -25,36 +25,44 @@ export class FoodDiary {
   protected foodService = inject(FoodService);
   protected foodTimeGrouped = signal<Record<string, FoodCartItem[]>>({});
   protected dates = signal<string[]>([]);
+  protected dailyTotals = signal<Record<string, { calories: number, protein: number }>>({});
 
   constructor() {
 
 
     effect(() => {
       const items = this.foodService.diary();
-      if(!items) return;
+      if (!items) return;
       const grouped: Record<string, FoodCartItem[]> = ({});
+      const totals: Record<string, { calories: number, protein: number }> = ({});
 
       for (const item of items) {
-        const dateKey = new Date(item.timestamp).toLocaleDateString('en-gb',{
-          day:'numeric',
-          month:'short',
-          year:'2-digit',
-          weekday:'short'
+        const dateKey = new Date(item.timestamp).toLocaleDateString('en-gb', {
+          day: 'numeric',
+          month: 'short',
+          year: '2-digit',
+          weekday: 'short'
         })
         if (!grouped[dateKey]) {
           grouped[dateKey] = []
+          totals[dateKey] = { calories: 0, protein: 0 };
         }
         grouped[dateKey].push(item);
+
+        totals[dateKey].calories += item.calories * item.quantity;
+        totals[dateKey].protein += item.protein * item.quantity;
       }
 
       this.foodTimeGrouped.set(grouped);
+      
+      this.dailyTotals.set(totals);
       this.dates.set(Object.keys(this.foodTimeGrouped()));
     })
 
 
   }
 
-  adjustDiaryItemQty(id:number,newQty:string){
-    return this.foodService.adjustDiaryItemQty(id,Number(newQty))
+  adjustDiaryItemQty(id: number, newQty: string) {
+    return this.foodService.adjustDiaryItemQty(id, Number(newQty))
   }
 }
