89b82a6cbcf43a114d0180c6888b03dede3b7177
revlienn <revnathalia@gmail.com>
2025-10-29 10:43:37 +0000

    Responsive homepage

--- client/src/app/foods/food-search-area/food-search-area.ts
+++ client/src/app/foods/food-search-area/food-search-area.ts
@@ -1,7 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import { Component, effect, inject, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FoodService } from '../../services/food-service';
 import { FoodSearchResult } from '../../../types/FoodSearchResult';
@@ -9,30 +9,43 @@ import { SearchResultList } from '../search-result-list/search-result-list';
 
 @Component({
   selector: 'app-food-search-area',
-  imports: [MatCardModule,MatInputModule,MatIconModule,ReactiveFormsModule,SearchResultList],
+  imports: [MatCardModule, MatInputModule, MatIconModule, ReactiveFormsModule, SearchResultList],
   templateUrl: './food-search-area.html',
   styleUrl: './food-search-area.css'
 })
 export class FoodSearchArea {
 
-  protected foodService=inject(FoodService);
-  searchResult=signal<FoodSearchResult|null>(null)
-  searched=signal<boolean>(false);
+  protected foodService = inject(FoodService);
+  protected screenMobile = signal<boolean>(false);
+  searchResult = signal<FoodSearchResult | null>(null)
+  searched = signal<boolean>(false);
 
-  searchTerm=new FormControl('apple',{
-    validators:[Validators.required,Validators.minLength(4)]
+  constructor() {
+    effect(() => {
+      window.addEventListener('resize', () => {
+        if(window.innerWidth<=768){
+          this.screenMobile.set(true);
+        }else{
+          this.screenMobile.set(false);
+        }
+      })
+    })
+  }
+
+  searchTerm = new FormControl('apple', {
+    validators: [Validators.required, Validators.minLength(4)]
   })
 
-  searchForm=new FormGroup({
-    searchTerm:this.searchTerm
+  searchForm = new FormGroup({
+    searchTerm: this.searchTerm
   })
 
-  search(){
-    if(this.searchForm.valid){
+  search() {
+    if (this.searchForm.valid) {
       this.searched.set(true);
-      const term=this.searchTerm.value?.trim();
-      if(term){
-        this.foodService.searchFood(term).subscribe((res)=>this.searchResult.set(res))
+      const term = this.searchTerm.value?.trim();
+      if (term) {
+        this.foodService.searchFood(term).subscribe((res) => this.searchResult.set(res))
 
       }
     }
